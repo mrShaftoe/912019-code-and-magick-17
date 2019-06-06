198cc93efd336b27fc4b64bdbccf347744735376
Eugene <mrshaftoe@gmail.com>
2019-06-07 00:30:45 +0300

    Names

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100; // Верхний левый угол облака для текста, координата X
+var CLOUD_Y = 10; // Верхний левый угол облака для текста, координата Y
+var CLOUD_WIDTH = 420; // Ширина облака
+var CLOUD_HEIGHT = 270; // Высота облака
 var GAP = 10;
 var CLOUD_COLOR = '#ffffff';
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var FONT = 'PT Mono';
 var FONT_SIZE = 16;
+var FONT_UNIT = 'px';
+var COLUMN_GAP = 50;
+var COLUMN_WIDTH = 40;
+
 
 var drawCloud = function (ctx, cloudCoordX, cloudCoordY, cloudWidth, cloudHeight, cloudColor) {
   ctx.fillStyle = cloudColor;
@@ -19,7 +24,12 @@ window.renderStatistics = function (ctx, names, times) {
   drawCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
 
   ctx.fillStyle = '#000';
+  ctx.font = FONT_SIZE + FONT_UNIT + ' ' + FONT;
   ctx.textAlign = 'center';
-  ctx.font = '16px "PT Mono"';
-  ctx.fillText('Hello world', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP + FONT_SIZE);
+  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP + FONT_SIZE);
+  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + 2 * (GAP + FONT_SIZE));
+  ctx.textAlign = 'left';
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], CLOUD_X + GAP / 2 + (i + 1) * COLUMN_GAP + i * COLUMN_WIDTH, CLOUD_Y + CLOUD_HEIGHT - GAP);
+  }
 };
