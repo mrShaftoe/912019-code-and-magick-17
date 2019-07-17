b1c1c8c427ee396e826af43ae15585182331f888
Eugene <mrshaftoe@gmail.com>
2019-07-17 14:33:33 +0300

    закрыл скобочку

--- js/avatar.js
+++ js/avatar.js
@@ -2,7 +2,7 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-  var fileChooser = document.querySelector('.upload input[type=file');
+  var fileChooser = document.querySelector('.upload input[type=file]');
   var preview = document.querySelector('.setup-user-pic');
 
   fileChooser.addEventListener('change', function () {
